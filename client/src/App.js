ea59a19730930f3d76ff2ca96714a9dc62b5d245
dp <davepollan@gmail.com>
2020-04-25 19:06:09 -0400

    front page structure done

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Books from "./pages/Books";
+import Nav from "./components/Nav";
+import './style.css';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Books />
+    </div>
+  );
+}
+
+export default App;
--- client/src/components/pages/Books.js
+++ client/src/components/pages/Books.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
-import DeleteBtn from "../components/DeleteBtn";
+import React, { useEffect, useState } from "react";
 import Jumbotron from "../components/Jumbotron";
+import DeleteBtn from "../components/DeleteBtn";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
@@ -26,33 +25,6 @@ function Books() {
       .catch(err => console.log(err));
   };
 
-  // Deletes a book from the database with a given id, then reloads books from the db
-  function deleteBook(id) {
-    API.deleteBook(id)
-      .then(res => loadBooks())
-      .catch(err => console.log(err));
-  }
-
-  // Handles updating component state when the user types into the input field
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
-  };
-
-  // When the form is submitted, use the API.saveBook method to save the book data
-  // Then reload books from the database
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    if (formObject.title && formObject.author) {
-      API.saveBook({
-        title: formObject.title,
-        author: formObject.author,
-        synopsis: formObject.synopsis
-      })
-        .then(res => loadBooks())
-        .catch(err => console.log(err));
-    }
-  };
 
     return (
       <Container fluid>
@@ -63,23 +35,23 @@ function Books() {
             </Jumbotron>
             <form>
               <Input
-                onChange={handleInputChange}
+                onChange={() => {}}
                 name="title"
                 placeholder="Title (required)"
               />
               <Input
-                onChange={handleInputChange}
+                onChange={() => {}}
                 name="author"
                 placeholder="Author (required)"
               />
               <TextArea
-                onChange={handleInputChange}
+                onChange={() => {}}
                 name="synopsis"
                 placeholder="Synopsis (Optional)"
               />
               <FormBtn
                 disabled={!(formObject.author && formObject.title)}
-                onClick={handleFormSubmit}
+                onClick={() => {}}
               >
                 Submit Book
               </FormBtn>
@@ -91,16 +63,18 @@ function Books() {
             </Jumbotron>
             {books.length ? (
               <List>
-                {books.map(book => (
-                  <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.author}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => deleteBook(book._id)} />
-                  </ListItem>
-                ))}
+                {books.map(book => {
+                  return (
+                    <ListItem key={book._id}>
+                      <a href={"/books/" + book._id}>
+                        <strong>
+                          {book.title} by {book.author}
+                        </strong>
+                      </a>
+                      <DeleteBtn onClick={() =>{}} />
+                    </ListItem>
+                  );
+                })}
               </List>
             ) : (
               <h3>No Results to Display</h3>
