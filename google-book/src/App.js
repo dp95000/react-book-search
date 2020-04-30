8e9d9940af803a240c5ce35d3a950ea2a2b77778
dp <davepollan@gmail.com>
2020-04-30 01:50:03 -0400

    last minor edits

--- google-book/src/App.js
+++ google-book/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import Header from './components/Nav/Header';
 
 function App() {
 
   const [book, setBook] = useState("");
   const [result, setResult] = useState([]);
-  // const [apiKey, setApiKey] = useState("AIzaSyBdZJEhwuXan7FNjMb6iFqiol49jC-ZRTU");
+  // const [apiKey, setApiKey] = useState("AIzaSyD2wPOmLrj6mxbVOXp9ZSOvIYOkO-xoERA");
 
 
   const handleChange = event => {
@@ -20,7 +21,7 @@ function App() {
     event.preventDefault();
     // console.log(event.target)
     console.log(book)
-    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=AIzaSyBdZJEhwuXan7FNjMb6iFqiol49jC-ZRTU")
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=AIzaSyD2wPOmLrj6mxbVOXp9ZSOvIYOkO-xoERA")
       .then(data => {
         const { items } = data.data;
         console.log("item",items);
@@ -33,9 +34,11 @@ function App() {
   /* Main Info is inside Volume Info */
 
   return (
-    <div className="container">
-      <h1></h1>
+    <div>
+      <Header />
+      <h3 className="intro">Enter a book title to start your search!</h3>
       <form>
+        <div className="container">
         <div className="form-group">
           <input type="text"
             onChange={handleChange}
@@ -47,9 +50,10 @@ function App() {
             type="submit"
             className="btn btn-danger">Search</button>
         </div>
+        </div>
       </form>
       {result.map(book => (
-        <div key={book._id}>
+        <div className="book-listing" key={book._id}>
         <a target="_blank" href={book.volumeInfo.previewLink}>
           <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
         </a>
---
+++ google-book/src/components/Nav/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <div>
+            <div className="container-fluid header">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>Google Book Search</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
